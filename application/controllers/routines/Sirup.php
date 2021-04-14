5e199883870c5922b671468bfa03cc8eb82db5dd
joshuanatan <ethernet.implementit.itsec@gmail.com>
2021-04-14 23:21:27 +0700

    Sirup
    Halfway done, sudah bisa insert ke table utama sirup, lalu ke table masing2 utk table within tables.
    
    sisa filtering dan show ke depan aja

--- application/controllers/routines/Sirup.php
+++ application/controllers/routines/Sirup.php
@@ -28,64 +28,66 @@ class Sirup extends CI_Controller{
         $response = curl_exec($curl);
         $response = json_decode($response,true);
         #print_r($response);
-        $id = $response["data"][0]["id"];
-        $pagu = $response["data"][0]["pagu"];
+        for($a = 0; $a<count($response["data"]); $a++){
+            $id = $response["data"][$a]["id"];
+            $pagu = $response["data"][$a]["pagu"];
 
-        $url = "https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/".$id;
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-        ));
-        $response = curl_exec($curl);
-        echo $response;
-        
-        $dom = new domDocument;
-        // load the html into the object
-        $dom->loadHTML($response);
-        $content = $dom->getElementById("detil");
-        $response = strval($content->nodeValue);
-        $response = preg_replace('/\s+/', ' ', $response);
-        $response = preg_replace('/\t+/', ' ', $response);
-        $response = preg_replace('/\n\r+/', '', $response);
-        #echo $response;
+            $url = "https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/".$id;
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+            ));
+            $response = curl_exec($curl);
+            echo $response;
+            
+            $dom = new domDocument;
+            // load the html into the object
+            $dom->loadHTML($response);
+            $content = $dom->getElementById("detil");
+            $response = strval($content->nodeValue);
+            $response = preg_replace('/\s+/', ' ', $response);
+            $response = preg_replace('/\t+/', ' ', $response);
+            $response = preg_replace('/\n\r+/', '', $response);
+            #echo $response;
 
-        #asumsi history paket aja yang beda
-        $kode_rup = explode("Nama Paket",explode("Kode RUP",$response)[1])[0];
-        $nama_paket = explode("Nama KLPD",explode("Nama Paket",$response)[1])[0];
-        $nama_klpd = explode("Satuan Kerja",explode("Nama KLPD",$response)[1])[0];
-        $satuan_kerja = explode("Tahun Anggaran",explode("Satuan Kerja",$response)[1])[0];
-        $tahun_anggaran = explode("Lokasi Pekerjaan",explode("Tahun Anggaran",$response)[1])[0];
-        $volume_pekerjaan = explode("Uraian Pekerjaan",explode("Volume Pekerjaan",$response)[1])[0];
-        $uraian_pekerjaan = explode("Spesifikasi Pekerjaan",explode("Uraian Pekerjaan",$response)[1])[0];
-        $spesifikasi_pekerjaan = explode("Produk Dalam Negeri",explode("Spesifikasi Pekerjaan",$response)[1])[0];
-        $produk_dalam_negri = explode("Usaha Kecil",explode("Produk Dalam Negeri",$response)[1])[0];
-        $usaha_kecil = explode("Pra DIPA / DPA",explode("Usaha Kecil",$response)[1])[0];
-        $pra_dipa = explode("Sumber Dana",explode("Pra DIPA / DPA",$response)[1])[0];
-        $jenis_pengadaan = explode("Total Pagu",explode("Jenis Pengadaan",$response)[1])[0];
-        $metode_pemilihan = explode("Pemanfaatan Barang/Jasa",explode("Metode Pemilihan",$response)[1])[0];
-        $tanggal_perbarui_paket = explode("Tanggal Perbarui Paket",$response)[1];
-        $lokasi_pekerjaan = explode("Volume Pekerjaan",explode("Lokasi Pekerjaan No. Provinsi Kabupaten/Kota Detail Lokasi",$response)[1])[0];
-        $sumber_dana = explode("Total Pagu",explode("Sumber Dana No. Sumber Dana T.A. KLPD MAK Pagu",$response)[1])[0];
-        $pemanfaatan_barang = explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",explode("Pemanfaatan Barang/Jasa Mulai Akhir",$response)[1])[0];
-        $jadwal_pelaksanaan = explode("Jadwal Pemilihan Penyedia Mulai Akhir",explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",$response)[1])[0];
-        $history_paket = "";
-        $pemilihan_penyedia = "";
-        if (strpos($response, 'History Paket') !== false) {
-            $history_paket = explode("Tanggal Perbarui Paket",explode("History Paket",$response)[1])[0];
-            $pemilihan_penyedia = explode("History Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
+            #asumsi history paket aja yang beda
+            $kode_rup = explode("Nama Paket",explode("Kode RUP",$response)[1])[0];
+            $nama_paket = explode("Nama KLPD",explode("Nama Paket",$response)[1])[0];
+            $nama_klpd = explode("Satuan Kerja",explode("Nama KLPD",$response)[1])[0];
+            $satuan_kerja = explode("Tahun Anggaran",explode("Satuan Kerja",$response)[1])[0];
+            $tahun_anggaran = explode("Lokasi Pekerjaan",explode("Tahun Anggaran",$response)[1])[0];
+            $volume_pekerjaan = explode("Uraian Pekerjaan",explode("Volume Pekerjaan",$response)[1])[0];
+            $uraian_pekerjaan = explode("Spesifikasi Pekerjaan",explode("Uraian Pekerjaan",$response)[1])[0];
+            $spesifikasi_pekerjaan = explode("Produk Dalam Negeri",explode("Spesifikasi Pekerjaan",$response)[1])[0];
+            $produk_dalam_negri = explode("Usaha Kecil",explode("Produk Dalam Negeri",$response)[1])[0];
+            $usaha_kecil = explode("Pra DIPA / DPA",explode("Usaha Kecil",$response)[1])[0];
+            $pra_dipa = explode("Sumber Dana",explode("Pra DIPA / DPA",$response)[1])[0];
+            $jenis_pengadaan = explode("Total Pagu",explode("Jenis Pengadaan",$response)[1])[0];
+            $metode_pemilihan = explode("Pemanfaatan Barang/Jasa",explode("Metode Pemilihan",$response)[1])[0];
+            $tanggal_perbarui_paket = explode("Tanggal Perbarui Paket",$response)[1];
+            $lokasi_pekerjaan = explode("Volume Pekerjaan",explode("Lokasi Pekerjaan No. Provinsi Kabupaten/Kota Detail Lokasi",$response)[1])[0];
+            $sumber_dana = explode("Total Pagu",explode("Sumber Dana No. Sumber Dana T.A. KLPD MAK Pagu",$response)[1])[0];
+            $pemanfaatan_barang = explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",explode("Pemanfaatan Barang/Jasa Mulai Akhir",$response)[1])[0];
+            $jadwal_pelaksanaan = explode("Jadwal Pemilihan Penyedia Mulai Akhir",explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",$response)[1])[0];
+            $history_paket = "";
+            $pemilihan_penyedia = "";
+            if (strpos($response, 'History Paket') !== false) {
+                $history_paket = explode("Tanggal Perbarui Paket",explode("History Paket",$response)[1])[0];
+                $pemilihan_penyedia = explode("History Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
+            }
+            else{
+                $pemilihan_penyedia = explode("Tanggal Perbarui Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
+            }
+            $id_user = 0;
+            if($this->session->id_user != ""){
+                $id_user = $this->session->id_user; 
+            }
         }
-        else{
-            $pemilihan_penyedia = explode("Tanggal Perbarui Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
-        }
-        
-        $preg = "/[0-9]\./";
-        $lokasi_pekerjaan = preg_split($preg,$lokasi_pekerjaan);
-        print_r($lokasi_pekerjaan);
     }
     public function load_sirup(){
         #query semua, ambil yg mau di cari dari dB
@@ -95,13 +97,15 @@ class Sirup extends CI_Controller{
         $search_keys = $this->m_pencarian_sirup->get_data();
         if($search_keys->num_rows() > 0){
             $search_keys = $search_keys->result_array();
-            for($a = 0; $a<count($search_keys); $a++){
+            for($a = 0; $a<1; $a++){
+                echo $a."<br/>";
+                $id_pk_pencarian_sirup = $search_keys[$a]["id_pk_pencarian_sirup"];
                 $pencarian_sirup_tahun = $search_keys[$a]["pencarian_sirup_tahun"];
                 $pencarian_sirup_frase = urlencode($search_keys[$a]["pencarian_sirup_frase"]);
                 $pencarian_sirup_jenis = $search_keys[$a]["pencarian_sirup_jenis"];
-                $amount = 100;
-
-                $url = "https://sirup.lkpp.go.id/sirup/ro/cari/search?tahunAnggaran=".$tahunAnggaran."&keyword=".$keyword."&jenisPengadaan=0&metodePengadaan=0&draw=1&order%5B0%5D%5Bcolumn%5D=5&order%5B0%5D%5Bdir%5D=DESC&start=0&length=".$amount."&search%5Bvalue%5D=&search%5Bregex%5D=false";
+                $amount = 1;
+        
+                $url = "https://sirup.lkpp.go.id/sirup/ro/cari/search?tahunAnggaran=".$pencarian_sirup_tahun."&keyword=".$pencarian_sirup_frase."&jenisPengadaan=$pencarian_sirup_jenis&metodePengadaan=0&draw=1&order%5B0%5D%5Bcolumn%5D=5&order%5B0%5D%5Bdir%5D=DESC&start=0&length=".$amount."&search%5Bvalue%5D=&search%5Bregex%5D=false";
 
                 $curl = curl_init();
                 curl_setopt_array($curl, array(
@@ -113,76 +117,129 @@ class Sirup extends CI_Controller{
                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                     CURLOPT_CUSTOMREQUEST => "GET",
                 ));
+                $info = curl_getinfo($curl);
                 $response = curl_exec($curl);
                 $response = json_decode($response,true);
+                #cek responsenya.   
                 print_r($response);
-                echo "------------------";
-                for($b = 0; $b<count($response["data"]); $b++){
-                    $this->m_sirup->insert_raw_result($response["data"][$b]["id"]);
+                $response_data_count = count($response["data"]);
+                for($b = 0; $b<$response_data_count; $b++){
+                    $id = $response["data"][$b]["id"];
+                    $pagu = $response["data"][$b]["pagu"];
+        
+                    $url = "https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/".$id;
+                    curl_setopt_array($curl, array(
+                        CURLOPT_URL => $url,
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_ENCODING => "",
+                        CURLOPT_MAXREDIRS => 10,
+                        CURLOPT_TIMEOUT => 30,
+                        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                        CURLOPT_CUSTOMREQUEST => "GET",
+                    ));
+                    $response = curl_exec($curl);
+                    $dom = new domDocument;
+                    // load the html into the object
+                    $dom->loadHTML($response);
+                    $content = $dom->getElementById("detil");
+                    $response = strval($content->nodeValue);
+                    $response = preg_replace('/\s+/', ' ', $response);
+                    $response = preg_replace('/\t+/', ' ', $response);
+                    $response = preg_replace('/\n\r+/', '', $response);
+                    #echo $response;
+                    $this->extract_data($response,$pagu,$id_pk_pencarian_sirup);
                 }
             }
         }
     }
-    public function load_detail_sirup(){
+    private function extract_data($response, $sirup_total, $id_fk_pencarian_sirup){
+        #asumsi history paket aja yang beda
+        $sirup_rup = explode("Nama Paket",explode("Kode RUP",$response)[1])[0];
+        $sirup_paket = explode("Nama KLPD",explode("Nama Paket",$response)[1])[0];
+        $sirup_klpd = explode("Satuan Kerja",explode("Nama KLPD",$response)[1])[0];
+        $sirup_satuan_kerja = explode("Tahun Anggaran",explode("Satuan Kerja",$response)[1])[0];
+        $sirup_tahun_anggaran = explode("Lokasi Pekerjaan",explode("Tahun Anggaran",$response)[1])[0];
+        $sirup_volume_pekerjaan = explode("Uraian Pekerjaan",explode("Volume Pekerjaan",$response)[1])[0];
+        $sirup_uraian_pekerjaan = explode("Spesifikasi Pekerjaan",explode("Uraian Pekerjaan",$response)[1])[0];
+        $sirup_spesifikasi_pekerjaan = explode("Produk Dalam Negeri",explode("Spesifikasi Pekerjaan",$response)[1])[0];
+        $sirup_produk_dalam_negri = explode("Usaha Kecil",explode("Produk Dalam Negeri",$response)[1])[0];
+        $sirup_usaha_kecil = explode("Pra DIPA / DPA",explode("Usaha Kecil",$response)[1])[0];
+        $sirup_pra_dipa = explode("Sumber Dana",explode("Pra DIPA / DPA",$response)[1])[0];
+        $sirup_jenis_pengadaan = explode("Total Pagu",explode("Jenis Pengadaan",$response)[1])[0];
+        $sirup_metode_pemilihan = explode("Pemanfaatan Barang/Jasa",explode("Metode Pemilihan",$response)[1])[0];
+        $sirup_histori_paket = "";
+        $sirup_tgl_perbarui_paket = explode("Tanggal Perbarui Paket",$response)[1];
+
+        $lokasi_pekerjaan = explode("Volume Pekerjaan",explode("Lokasi Pekerjaan No. Provinsi Kabupaten/Kota Detail Lokasi",$response)[1])[0];
+        $sumber_dana = explode("Total Pagu",explode("Sumber Dana No. Sumber Dana T.A. KLPD MAK Pagu",$response)[1])[0];
+        #echo "sumberdana atas:".$sumber_dana;
+        $pemanfaatan_barang = explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",explode("Pemanfaatan Barang/Jasa Mulai Akhir",$response)[1])[0];
+        $jadwal_pelaksanaan = explode("Jadwal Pemilihan Penyedia Mulai Akhir",explode("Jadwal Pelaksanaan Kontrak Mulai Akhir",$response)[1])[0];
+        $pemilihan_penyedia = "";
+        if (strpos($response, 'History Paket') !== false) {
+            $sirup_histori_paket = explode("Tanggal Perbarui Paket",explode("History Paket",$response)[1])[0];
+            $pemilihan_penyedia = explode("History Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
+        }
+        else{
+            $pemilihan_penyedia = explode("Tanggal Perbarui Paket",explode("Jadwal Pemilihan Penyedia Mulai Akhir",$response)[1])[0];
+        }
+        $sirup_id_create = 0;
+        if($this->session->id_user != ""){
+            $sirup_id_create = $this->session->id_user; 
+        }
         $this->load->model("m_sirup");
-        $result = $this->m_sirup->load_sirup();
-        $result = $result->result_array();
-        $curl = curl_init();
-        for($a = 0; $a<count($result); $a++){
-            $sirup = $result[$a]["sirup"];
-            $url = "https://sirup.lkpp.go.id/sirup/home/detailPaketPenyediaPublic2017/".$sirup;
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-            ));
-            $response = curl_exec($curl);
-            if($response){
-                echo htmlentities($response);
-                #$this->extract_data($response);
+        $id_pk_sirup = $this->m_sirup->insert($sirup_rup,$sirup_paket,$sirup_klpd,$sirup_satuan_kerja,$sirup_tahun_anggaran,$sirup_volume_pekerjaan,$sirup_uraian_pekerjaan,$sirup_spesifikasi_pekerjaan,$sirup_produk_dalam_negri,$sirup_usaha_kecil,$sirup_pra_dipa,$sirup_jenis_pengadaan,$sirup_total,$sirup_metode_pemilihan,$sirup_histori_paket,$sirup_tgl_perbarui_paket,$sirup_id_create,$id_fk_pencarian_sirup);
+
+        $preg = '/[0-9]\.\ /';
+        $lokasi_pekerjaan = preg_split($preg,$lokasi_pekerjaan);
+        print_r($lokasi_pekerjaan);
+        for($a = 0; $a<count($lokasi_pekerjaan); $a++){
+            if($lokasi_pekerjaan[$a] == ""){
+                continue;
             }
+            #start dari 1 karena 0 nya pasti blank. Cth data 1. abcdef, nah karena split by 1. , jadinya abcdefnya itu ada di index 1
+            #print_r($lokasi_pekerjaan);
+            $this->m_sirup->insert_lokasi_pekerjaan($lokasi_pekerjaan[$a],$id_pk_sirup);
+        }
+        $sumber_dana = preg_split($preg,$sumber_dana);
+        print_r($sumber_dana);
+        for($a = 0; $a<count($sumber_dana); $a++){
+            if($lokasi_pekerjaan[$a] == ""){
+                continue;
+            }
+            #start dari 1 karena 0 nya pasti blank. Cth data 1. abcdef, nah karena split by 1. , jadinya abcdefnya itu ada di index 1
+            #print_r($sumber_dana);
+            $this->m_sirup->insert_sumber_dana($sumber_dana[$a],$id_pk_sirup);
+        }
+        $pemanfaatan_barang = preg_split($preg,$pemanfaatan_barang);
+        print_r($pemanfaatan_barang);
+        for($a = 0; $a<count($pemanfaatan_barang); $a++){
+            if($lokasi_pekerjaan[$a] == ""){
+                continue;
+            }
+            #start dari 1 karena 0 nya pasti blank. Cth data 1. abcdef, nah karena split by 1. , jadinya abcdefnya itu ada di index 1
+            #print_r($pemanfaatan_barang);
+            $this->m_sirup->insert_pemanfaatan_barang($pemanfaatan_barang[$a],$id_pk_sirup);
+        }
+        $jadwal_pelaksanaan = preg_split($preg,$jadwal_pelaksanaan);
+        print_r($jadwal_pelaksanaan);
+        for($a = 0; $a<count($jadwal_pelaksanaan); $a++){
+            if($lokasi_pekerjaan[$a] == ""){
+                continue;
+            }
+            #start dari 1 karena 0 nya pasti blank. Cth data 1. abcdef, nah karena split by 1. , jadinya abcdefnya itu ada di index 1
+            #print_r($jadwal_pelaksanaan);
+            $this->m_sirup->insert_jadwal_pelaksanaan($jadwal_pelaksanaan[$a],$id_pk_sirup);
+        }
+        $pemilihan_penyedia = preg_split($preg,$pemilihan_penyedia);
+        print_r($pemilihan_penyedia);
+        for($a = 0; $a<count($pemilihan_penyedia); $a++){
+            if($lokasi_pekerjaan[$a] == ""){
+                continue;
+            }
+            #start dari 1 karena 0 nya pasti blank. Cth data 1. abcdef, nah karena split by 1. , jadinya abcdefnya itu ada di index 1
+            #print_r($pemilihan_penyedia);
+            $this->m_sirup->insert_pemilihan_penyedia($pemilihan_penyedia[$a],$id_pk_sirup);
         }
     }
-    private function extract_data2($response){
-        $dom = new domDocument;
-        // load the html into the object
-        $dom->loadHTML($response);
-        $content = $dom->getElementsByTagName("tr");
-        foreach ($content as $a) {
-            $test = $a->getElementsByTagName("td");
-            $counter = 0;
-            print_r($test);
-            foreach ($test as $b){
-                echo $b->nodeValue;
-                $counter++;
-            }
-            echo "<br/>";
-        }   
-    }
-    // private function extract_data($response){
-    //     error_reporting(0);
-    //     $dom = new domDocument;
-    //     // load the html into the object
-    //     $dom->loadHTML($response);
-    //     $content = $dom->getElementsByTagName("tr");
-    //     foreach($content as $a){
-    //         $child = $a->childNodes;
-    //         $temp_array = array();
-    //         $flag = 0;
-    //         foreach($child as $b){
-    //             if($b->nodeValue && $b->nodeType == 1){
-    //                 $temp_array[$flag] = $b->nodeValue;
-    //                 $flag++;
-    //             }
-    //             if($flag % 2 == 0){
-    //                 echo $temp_array[0]." - ".$temp_array[1];
-    //                 echo "<br/>";
-    //             }
-    //         }
-    //     }
-    // }
 }
--- application/models/M_sirup.php
+++ application/models/M_sirup.php
@@ -1,21 +1,119 @@
 <?php
+/*
+    drop table if exists mstr_sirup;
+    create table mstr_sirup(
+        id_pk_sirup int primary key auto_increment,
+        sirup_rup varchar(100),
+        sirup_paket text,
+        sirup_klpd varchar(100),
+        sirup_satuan_kerja varchar(100),
+        sirup_tahun_anggaran int,
+        sirup_volume_pekerjaan varchar(300),
+        sirup_uraian_pekerjaan varchar(300),
+        sirup_spesifikasi_pekerjaan varchar(300),
+        sirup_produk_dalam_negri varchar(15),
+        sirup_usaha_kecil varchar(15),
+        sirup_pra_dipa varchar(15),
+        sirup_jenis_pengadaan varchar(300),
+        sirup_total int,
+        sirup_metode_pemilihan varchar(100),
+        sirup_histori_paket varchar(100),
+        sirup_tgl_perbarui_paket varchar(100),
+        sirup_status varchar(30),
+        sirup_tgl_create datetime,
+        sirup_tgl_update datetime,
+        sirup_tgl_delete datetime,
+        sirup_id_create int,
+        sirup_id_update int,
+        sirup_id_delete int,
+        id_fk_pencarian_sirup int
+    );
+*/
 class M_sirup extends CI_Model{
-    public function load_search_key(){
-        $sql = "select id_pk_sirup_search_key, sirup_search_key from mstr_sirup_search_key where status_sirup_search_key = 'aktif'";
-        $result = executeQuery($sql);
-        return $result;
-    }
-    public function insert_raw_result($sirup){
+    private $id_pk_sirup;
+    private $sirup_rup;
+    private $sirup_paket;
+    private $sirup_klpd;
+    private $sirup_satuan_kerja;
+    private $sirup_tahun_anggaran;
+    private $sirup_volume_pekerjaan;
+    private $sirup_uraian_pekerjaan;
+    private $sirup_spesifikasi_pekerjaan;
+    private $sirup_produk_dalam_negri;
+    private $sirup_usaha_kecil;
+    private $sirup_pra_dipa;
+    private $sirup_jenis_pengadaan;
+    private $sirup_total;
+    private $sirup_metode_pemilihan;
+    private $sirup_histori_paket;
+    private $sirup_tgl_perbarui_paket;
+    private $sirup_status;
+    private $sirup_tgl_create;
+    private $sirup_tgl_update;
+    private $sirup_tgl_delete;
+    private $sirup_id_create;
+    private $sirup_id_update;
+    private $sirup_id_delete;
+    private $id_fk_pencarian_sirup;
+    
+    public function insert($sirup_rup,$sirup_paket,$sirup_klpd,$sirup_satuan_kerja,$sirup_tahun_anggaran,$sirup_volume_pekerjaan,$sirup_uraian_pekerjaan,$sirup_spesifikasi_pekerjaan,$sirup_produk_dalam_negri,$sirup_usaha_kecil,$sirup_pra_dipa,$sirup_jenis_pengadaan,$sirup_total,$sirup_metode_pemilihan,$sirup_histori_paket,$sirup_tgl_perbarui_paket,$sirup_id_create,$id_fk_pencarian_sirup){
         $data = array(
-            "sirup" => $sirup
+            "sirup_rup" => $sirup_rup,
+            "sirup_paket" => $sirup_paket,
+            "sirup_klpd" => $sirup_klpd,
+            "sirup_satuan_kerja" => $sirup_satuan_kerja,
+            "sirup_tahun_anggaran" => $sirup_tahun_anggaran,
+            "sirup_volume_pekerjaan" => $sirup_volume_pekerjaan,
+            "sirup_uraian_pekerjaan" => $sirup_uraian_pekerjaan,
+            "sirup_spesifikasi_pekerjaan" => $sirup_spesifikasi_pekerjaan,
+            "sirup_produk_dalam_negri" => $sirup_produk_dalam_negri,
+            "sirup_usaha_kecil" => $sirup_usaha_kecil,
+            "sirup_pra_dipa" => $sirup_pra_dipa,
+            "sirup_jenis_pengadaan" => $sirup_jenis_pengadaan,
+            "sirup_total" => $sirup_total,
+            "sirup_metode_pemilihan" => $sirup_metode_pemilihan,
+            "sirup_histori_paket" => $sirup_histori_paket,
+            "sirup_tgl_perbarui_paket" => $sirup_tgl_perbarui_paket,
+            "sirup_status" => "aktif",
+            "sirup_tgl_create" => date("Y-m-d H:i:s"),
+            "sirup_id_create" => $sirup_id_create,
+            "id_fk_pencarian_sirup" => $id_fk_pencarian_sirup
         );
-        insertRow("poc_sirup",$data);
+        return insertRow("mstr_sirup",$data);
     }
-    public function insert_key_word($keyword){
+    public function insert_lokasi_pekerjaan($data,$id_fk_sirup){
         $data = array(
-            "sirup_search_key" => $keyword,
-            "status_sirup_search_key" => "aktif"
+            "lokasi_pekerjaan" => $data,
+            "id_fk_sirup" => $id_fk_sirup
         );
-        insertRow("mstr_sirup_search_key",$data);
+        insertRow("tbl_sirup_lokasi_pekerjaan",$data);
+    }
+    public function insert_sumber_dana($data,$id_fk_sirup){
+        $data = array(
+            "sumber_dana" => $data,
+            "id_fk_sirup" => $id_fk_sirup
+        );
+        insertRow("tbl_sirup_sumber_dana",$data);
+    }
+    public function insert_pemanfaatan_barang($data,$id_fk_sirup){
+        $data = array(
+            "pemanfaatan_barang" => $data,
+            "id_fk_sirup" => $id_fk_sirup
+        );
+        insertRow("tbl_sirup_pemanfaatan_barang",$data);
+    }
+    public function insert_jadwal_pelaksanaan($data,$id_fk_sirup){
+        $data = array(
+            "jadwal_pelaksanaan" => $data,
+            "id_fk_sirup" => $id_fk_sirup
+        );
+        insertRow("tbl_sirup_jadwal_pelaksanaan",$data);
+    }
+    public function insert_pemilihan_penyedia($data,$id_fk_sirup){
+        $data = array(
+            "pemilihan_penyedia" => $data,
+            "id_fk_sirup" => $id_fk_sirup
+        );
+        insertRow("tbl_sirup_pemilihan_penyedia",$data);
     }
 }
