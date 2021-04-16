4089eda4066c002ab8d27714f96989ff89622f45
erickgenn <erickgennady@gmail.com>
2021-04-16 22:03:35 +0700

    Update User.php

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,7 +6,45 @@ class User extends CI_Controller{
         parent::__construct();
     }
     public function index(){
-        $this->load->view();
+        $this->load->model("m_user");
+        $this->load->model("m_kabupaten");
+        $result_user = $this->m_user->get_user();
+        $result_kabupaten = $this->m_kabupaten->get_kabupaten();
+
+        $data = array (
+          "data_user" => $result_user->result_array(),
+          "data_kabupaten" => $result_kabupaten->result_array(),
+        );
+
+        $this->load->view("user/index", $data);
     }
-    
+
+    public function insert() {
+      //$temp_id_fk_user_level = $this->input->post('');
+      $temp_user_username = $this->input->post('username');
+      $temp_user_password = $this->input->post('password');
+      $temp_user_email = $this->input->post('email');
+      $temp_user_telepon = $this->input->post('telepon');
+  		$this->load->model("m_user");
+  		$this->m_user->test_insert($temp_user_username, $temp_user_password, $temp_user_email, $temp_user_telepon);
+      Redirect("user/index");
+  	}
+
+    public function delete($id_pk_user) {
+      $this->load->model("m_user");
+      $this->m_user->delete_user($id_pk_user);
+      Redirect("user/index");
+    }
+
+    public function edit() {
+      $temp_id_pk_produk = $this->input->post('id_user');
+      $temp_user_username = $this->input->post('username');
+      $temp_user_password = $this->input->post('password');
+      $temp_user_email = $this->input->post('email');
+      $temp_user_telepon = $this->input->post('telepon');
+      $this->load->model("m_user");
+      $this->m_user->test_edit($temp_id_pk_produk, $temp_user_username, $temp_user_password, $temp_user_email, $temp_user_telepon);
+      Redirect("user/index");
+    }
+
 }
