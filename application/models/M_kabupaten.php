f9f722d8d8d5d24a285e222df66a9242adbbe22d
erickgenn <erickgennady@gmail.com>
2021-04-20 15:25:32 +0700

    Update M_kabupaten.php

--- application/models/M_kabupaten.php
+++ application/models/M_kabupaten.php
@@ -8,7 +8,7 @@ date_default_timezone_set("Asia/Jakarta");
 class M_kabupaten extends CI_Model{
 
   public function get_kabupaten(){
-    $sql = "select kabupaten_nama from mstr_kabupaten";
+    $sql = "select id_pk_kabupaten, kabupaten_nama from mstr_kabupaten";
     $result = $this->db->query($sql);
     return $result;
   }
