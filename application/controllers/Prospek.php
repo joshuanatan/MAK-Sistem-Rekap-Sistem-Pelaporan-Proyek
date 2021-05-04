935727f847dc2f13dd7630d8980d1ea10e547372
erickgenn <erickgennady@gmail.com>
2021-05-04 17:28:35 +0700

    Update Prospek.php

--- application/controllers/Prospek.php
+++ application/controllers/Prospek.php
@@ -1,10 +1,20 @@
 <?php
 class Prospek extends CI_Controller{
     public function index(){
+
+
         $this->load->view("prospek/index");
     }
     public function tambah_prospek(){
-        $this->load->view("prospek/tambah_prospek");
+        $this->load->model("m_prospek");
+
+        $result1 = $this->m_prospek->get_rs();
+        $result2 = $this->m_prospek->get_produk();
+        $data = array(
+          'datars' => $result1->result_array(),
+          'dataproduk' => $result2->result_array(),
+        );
+        $this->load->view("prospek/tambah_prospek", $data);
     }
 }
 ?>
