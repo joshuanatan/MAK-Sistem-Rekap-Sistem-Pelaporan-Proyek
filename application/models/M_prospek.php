e31ea14738ea9c913616c4f0310ac8f052b737d2
erickgenn <erickgennady@gmail.com>
2021-05-14 18:28:33 +0700

    Update M_prospek.php

--- application/models/M_prospek.php
+++ application/models/M_prospek.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Jakarta");
 class M_prospek extends CI_Model{
 
      public function get_prospek(){
-       $sql = "SELECT id_pk_prospek, mstr_provinsi.provinsi_nama as nama_provinsi, mstr_kabupaten.kabupaten_nama as nama_kabupaten, mstr_rs.rs_nama as nama_rs, prospek_principle, total_price_prospek, notes_kompetitor, notes_prospek, funnel, estimasi_pembelian, prospek_status
+       $sql = "SELECT id_pk_prospek, mstr_provinsi.provinsi_nama as nama_provinsi, mstr_kabupaten.kabupaten_nama as nama_kabupaten, mstr_rs.rs_nama as nama_rs, prospek_principle, total_price_prospek, notes_kompetitor, notes_prospek, no_sirup, no_ekatalog, funnel, funnel_percentage, estimasi_pembelian, note_loss, prospek_status
        FROM mstr_prospek
        INNER JOIN mstr_rs on mstr_prospek.id_fk_rs = mstr_rs.id_pk_rs
        INNER JOIN mstr_provinsi on mstr_provinsi.id_pk_provinsi = mstr_prospek.id_fk_provinsi
@@ -14,7 +14,7 @@ class M_prospek extends CI_Model{
      }
 
      public function get_prospek_produk($id_fk_prospek){
-       $sql = "SELECT mstr_produk.produk_nama as nama_produk, detail_prospek_quantity, mstr_produk.produk_price_list as harga_produk, detail_prospek_keterangan, detail_prospek_status
+       $sql = "SELECT id_pk_prospek_produk, tbl_prospek_produk.id_fk_produk, mstr_produk.produk_nama as nama_produk, detail_prospek_quantity, detail_prospek_keterangan, detail_prospek_status, prospek_produk_price
        FROM tbl_prospek_produk
        INNER JOIN mstr_produk on tbl_prospek_produk.id_pk_prospek_produk = mstr_produk.id_pk_produk
        WHERE id_fk_prospek = $id_fk_prospek AND detail_prospek_status='aktif'";
@@ -56,7 +56,12 @@ class M_prospek extends CI_Model{
      }
 
      public function get_produk(){
-       $sql = "SELECT id_pk_produk, produk_nama,produk_price_list FROM mstr_produk WHERE produk_status = 'aktif'";
+       $sql = "SELECT * FROM mstr_produk WHERE produk_status = 'aktif'";
+       return executeQuery($sql);
+     }
+
+     public function get_data_price($id_pk_produk){
+       $sql = "SELECT id_pk_produk, produk_price_list, produk_harga_ekat FROM mstr_produk WHERE id_pk_produk = $id_pk_produk AND produk_status = 'aktif'";
        return executeQuery($sql);
      }
 
@@ -259,17 +264,276 @@ class M_prospek extends CI_Model{
        return insertRow("mstr_prospek",$data);
      }
 
-     public function insert_produk_prospek($id_fk_prospek, $id_fk_produk, $detail_prospek_quantity, $detail_prospek_keterangan){
+     public function insert_produk_prospek($id_fk_prospek, $id_fk_produk, $prospek_produk_price, $detail_prospek_quantity, $detail_prospek_keterangan){
 
        $data = array(
          "id_fk_prospek"=>$id_fk_prospek,
          "id_fk_produk"=>$id_fk_produk,
+         "prospek_produk_price" => $prospek_produk_price,
          "detail_prospek_quantity"=>$detail_prospek_quantity,
          "detail_prospek_keterangan"=>$detail_prospek_keterangan,
          "detail_prospek_status"=>"aktif"
        );
        $this->db->insert("tbl_prospek_produk",$data);
      }
+
+     public function insert_total_price($id_pk_prospek, $total){
+       $where =array(
+         "id_pk_prospek"=> $id_pk_prospek
+       );
+
+       $data = array(
+         "total_price_prospek" => $total
+       );
+       return updateRow("mstr_prospek", $data, $where);
+     }
+
+     //Edit
+
+     public function edit_prospek_se($id_pk_prospek, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+
+       $data = array(
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_se_prospek($id_pk_prospek, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $funnel_percentage) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+
+       $data = array(
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "funnel_percentage"=>$funnel_percentage,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_se_loss($id_pk_prospek, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $note_loss) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "note_loss"=>$note_loss,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_asm($id_pk_prospek, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_asm_prospek($id_pk_prospek, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $funnel_percentage) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "funnel_percentage"=>$funnel_percentage,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_asm_loss($id_pk_prospek, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $note_loss) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "note_loss"=>$note_loss,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_asm_sirup($id_pk_prospek, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $no_sirup) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+
+       $data = array(
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "no_sirup"=>$no_sirup,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_sm($id_pk_prospek, $id_fk_provinsi, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_provinsi"=>$id_fk_provinsi,
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_sm_prospek($id_pk_prospek, $id_fk_provinsi, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $funnel_percentage) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_provinsi"=>$id_fk_provinsi,
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "funnel_percentage"=>$funnel_percentage,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_sm_loss($id_pk_prospek, $id_fk_provinsi, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $note_loss) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_provinsi"=>$id_fk_provinsi,
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "note_loss"=>$note_loss,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_prospek_sm_ekatalog($id_pk_prospek, $id_fk_provinsi, $id_fk_kabupaten, $id_fk_rs, $prospek_principle, $total_price_prospek, $notes_kompetitor, $notes_prospek, $estimasi_pembelian, $funnel, $no_ekatalog) {
+       $where = array(
+         "id_pk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_provinsi"=>$id_fk_provinsi,
+         "id_fk_kabupaten"=>$id_fk_kabupaten,
+         "id_fk_rs"=>$id_fk_rs,
+         "prospek_principle"=>$prospek_principle,
+         "total_price_prospek"=>$total_price_prospek,
+         "notes_kompetitor"=>$notes_kompetitor,
+         "notes_prospek"=>$notes_prospek,
+         "estimasi_pembelian"=>$estimasi_pembelian,
+         "funnel"=>$funnel,
+         "no_ekatalog"=>$no_ekatalog,
+         "prospek_status"=>"aktif"
+       );
+       return updateRow("mstr_prospek",$data, $where);
+     }
+
+     public function edit_produk_prospek($id_pk_prospek, $id_fk_prospek, $id_fk_produk, $prospek_produk_price, $detail_prospek_quantity, $detail_prospek_keterangan){
+       $where = array(
+         "id_fk_prospek" => $id_pk_prospek
+       );
+       $data = array(
+         "id_fk_produk"=>$id_fk_produk,
+         "detail_prospek_quantity"=>$detail_prospek_quantity,
+         "prospek_produk_price" => $prospek_produk_price,
+         "detail_prospek_keterangan"=>$detail_prospek_keterangan,
+         "detail_prospek_status"=>"aktif"
+       );
+       return updateRow("tbl_prospek_produk",$data, $where);
+     }
+
+    public function edit_get_prospek($id_pk_prospek){
+      $sql = "SELECT id_pk_prospek, mstr_prospek.id_fk_provinsi, mstr_provinsi.provinsi_nama as nama_provinsi, mstr_prospek.id_fk_kabupaten, mstr_kabupaten.kabupaten_nama as nama_kabupaten, mstr_prospek.id_fk_rs, mstr_rs.rs_nama as nama_rs, prospek_principle, total_price_prospek, notes_kompetitor, notes_prospek, funnel, funnel_percentage, no_sirup, no_ekatalog, note_loss, estimasi_pembelian, prospek_status
+      FROM mstr_prospek
+      INNER JOIN mstr_rs on mstr_prospek.id_fk_rs = mstr_rs.id_pk_rs
+      INNER JOIN mstr_provinsi on mstr_provinsi.id_pk_provinsi = mstr_prospek.id_fk_provinsi
+      INNER JOIN mstr_kabupaten on mstr_kabupaten.id_pk_kabupaten = mstr_prospek.id_fk_kabupaten
+      WHERE prospek_status='aktif' AND mstr_prospek.id_pk_prospek = $id_pk_prospek";
+      $result = $this->db->query($sql);
+      return $result;
+    }
+
+    public function delete_prospek_produk($id){
+      $where = array(
+        "id_pk_prospek_produk" => $id
+      );
+      $data = array(
+        "detail_prospek_status"=>"nonaktif"
+      );
+      return updateRow("tbl_prospek_produk",$data, $where);
+    }
 
     public function search($kolom_pengurutan,$arah_kolom_pengurutan,$pencarian_phrase,$kolom_pencarian,$current_page){
       $search_query = "";
