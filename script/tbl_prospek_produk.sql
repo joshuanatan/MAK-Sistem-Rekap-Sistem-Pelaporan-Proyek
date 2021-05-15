546207bae79556501261e32d8ee145f4f74f835c
erickgenn <erickgennady@gmail.com>
2021-05-15 19:47:24 +0700

    Update tbl_prospek_produk.sql

--- script/tbl_prospek_produk.sql
+++ script/tbl_prospek_produk.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 08, 2021 at 06:28 PM
+-- Generation Time: May 15, 2021 at 02:44 PM
 -- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,6 +31,7 @@ CREATE TABLE `tbl_prospek_produk` (
   `id_pk_prospek_produk` int(11) NOT NULL,
   `id_fk_prospek` int(11) NOT NULL,
   `id_fk_produk` int(11) NOT NULL,
+  `prospek_produk_price` varchar(1000) NOT NULL,
   `detail_prospek_quantity` varchar(100) NOT NULL,
   `detail_prospek_keterangan` varchar(1000) NOT NULL,
   `detail_prospek_status` varchar(100) NOT NULL
@@ -40,9 +41,14 @@ CREATE TABLE `tbl_prospek_produk` (
 -- Dumping data for table `tbl_prospek_produk`
 --
 
-INSERT INTO `tbl_prospek_produk` (`id_pk_prospek_produk`, `id_fk_prospek`, `id_fk_produk`, `detail_prospek_quantity`, `detail_prospek_keterangan`, `detail_prospek_status`) VALUES
-(1, 2, 2, '12', 'TEST1', 'aktif'),
-(2, 2, 5, '1211', 'TEST2', 'aktif');
+INSERT INTO `tbl_prospek_produk` (`id_pk_prospek_produk`, `id_fk_prospek`, `id_fk_produk`, `prospek_produk_price`, `detail_prospek_quantity`, `detail_prospek_keterangan`, `detail_prospek_status`) VALUES
+(1, 2, 2, '10', '12', 'TEST1', 'aktif'),
+(2, 2, 5, '12', '1211', 'TEST2', 'aktif'),
+(3, 16, 14, '3241', '54', '44444444', 'aktif'),
+(4, 16, 14, '1234', '54', '44444444', 'aktif'),
+(5, 16, 14, '123444', '54', '44444444', 'aktif'),
+(6, 18, 6, '1231234444', '1', '1234', 'aktif'),
+(7, 19, 10, '1222', '2', 'abcdef', 'aktif');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +68,7 @@ ALTER TABLE `tbl_prospek_produk`
 -- AUTO_INCREMENT for table `tbl_prospek_produk`
 --
 ALTER TABLE `tbl_prospek_produk`
-  MODIFY `id_pk_prospek_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pk_prospek_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
