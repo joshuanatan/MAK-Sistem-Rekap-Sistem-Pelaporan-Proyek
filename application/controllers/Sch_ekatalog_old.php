0cc3d1b385c119799eb2181ba3a02297cd5c0f12
Joshua Natan <96916832+joshua-wijaya@users.noreply.github.com>
2022-05-08 22:04:24 +0700

    done

--- application/controllers/Sch_ekatalog_old.php
+++ application/controllers/Sch_ekatalog_old.php
@@ -4,7 +4,6 @@ error_reporting(0);
 class Sch_ekatalog extends CI_Controller
 {
   private $login_cookies;
-  private $jwt;
   public function index()
   {
     if ($this->input->get("login")) {
@@ -47,7 +46,7 @@ class Sch_ekatalog extends CI_Controller
     $authentication_code = $matches[0];
 
     $username = "makhe2012";
-    $password = "M@kpolaris2021";
+    $password = "m@kit2008";
     $curl = curl_init();
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://e-katalog.lkpp.go.id/admin.userctr/loginpenyediasubmit",
@@ -61,109 +60,28 @@ class Sch_ekatalog extends CI_Controller
       CURLOPT_POSTFIELDS => "username=" . $username . "&password=" . $password . "&authenticityToken=" . $authentication_code,
     ));
     $response = curl_exec($curl);
-    // echo $response;
-    $key = 'set-cookie:';
+    echo $response;
+    $key = 'Set-Cookie:';
     if (strpos($response, $key) !== false) {
       echo 'true';
     }
     #print_r(explode("Set-Cookie:", $response));
-    $response = explode("set-cookie: ", $response, 2);
-    print_r($response[1]); //careful ini sangat bergantung pada responsenya
-    $response[1] = str_replace("set-cookie: ", ";", $response[1]);
-    $response[1] = str_replace(array("\n", "\r"), "", $response[1]);
-    echo "<br/><br/>";
-    // echo $response;
-    // print_r($response);
-    // $this->login_cookies = explode(";", $response[2])[0];
-    $this->login_cookies = $response[1];
-  }
-  public function get_jwt()
-  {
-    $this->login();
-    echo "login cookies: " . $this->login_cookies;
-    $url = "https://e-katalog.lkpp.go.id/home";
-
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => $url,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
-        "Cookie: " . $this->login_cookies
-      )
-    ));
-    $response = curl_exec($curl); #string
-    // echo $response;
-    preg_match('/(ey[a-zA-Z0-9_-]{1,99}\.){2}[a-zA-Z0-9_-]{1,99}/', $response, $matches);
-    $jwt = $matches[0];
-    $this->jwt = $jwt;
+    $response = explode("Set-Cookie:", $response); 
+    #echo $response;
+    $this->login_cookies = explode(";", $response[2])[0];
   }
   public function get_data()
   {
-    if ($this->login_cookies == "" || $this->jwt == "") {
-      $this->get_jwt();
+    if ($this->login_cookies == "") {
+      $this->login();
     }
-    $url = "https://e-katalog.lkpp.go.id/api/purchasing/api/v2/epurchasing?timer=Th042022022752&page=0&perPage=1000&limit=1000&total=0&tanggalDibuat=&activityId=&kompetisiId=";
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => $url,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
-        "Cookie: " . $this->login_cookies,
-        "Jwt-Token: " . $this->jwt,
-      )
-    ));
-    $response = (curl_exec($curl)); #string
-    $response = json_decode($response, true);
-    $response = $response["data"]["list"];
-    for ($a = 0; $a < count($response); $a++) {
-      $where = array(
-        "ekatalog_id_paket" => $response[$a]["no_paket"]
-      );
-      deleteRow("mstr_ekatalog", $where);
-      $data = array(
-        "ekatalog_komoditas" => $response[$a]["nama_paket"],
-        "ekatalog_id_paket" => $response[$a]["no_paket"],
-        "ekatalog_nama_paket" => $response[$a]["nama_paket"],
-        "ekatalog_instansi" => $response[$a]["kldi"]["nama"],
-        "ekatalog_satuan_kerja" => $response[$a]["satker"]["nama"],
-        "ekatalog_npwp_satuan_kerja" => $response[$a]["satuan_kerja_npwp"],
-        "ekatalog_alamat_satuan_kerja" => $response[$a]["satuan_kerja_alamat"],
-        "ekatalog_alamat_pengiriman" => $response[$a]["pengiriman_alamat"],
-        "ekatalog_tahun_anggaran" => $response[$a]["tahun_anggaran"],
-        "ekatalog_total_produk" => $response[$a]["total_product"],
-        "ekatalog_status_paket" => $response[$a]["latest_status"]["status_paket"],
-        "ekatalog_status" => "aktif",
-        "ekatalog_tgl_create" => date("Y-m-d H:i:s"),
-        "ekatalog_id_create" => 0,
-        "ekatalog_id_sirup" => $response[$a]["rup_id"],
-        "ekatalog_id_sirup_online" => $response[$a]["rup_id"],
-        "ekatalog_komoditas_online" => $response[$a]["nama_paket"],
-        "ekatalog_id_paket_online" => $response[$a]["no_paket"],
-        "ekatalog_nama_paket_online" => $response[$a]["nama_paket"],
-        "ekatalog_instansi_online" => $response[$a]["kldi"]["nama"],
-        "ekatalog_satuan_kerja_online" => $response[$a]["satker"]["nama"],
-        "ekatalog_npwp_satuan_kerja_online" => $response[$a]["satuan_kerja_npwp"],
-        "ekatalog_alamat_satuan_kerja_online" => $response[$a]["satuan_kerja_alamat"],
-        "ekatalog_alamat_pengiriman_online" => $response[$a]["pengiriman_alamat"],
-        "ekatalog_tahun_anggaran_online" => $response[$a]["tahun_anggaran"],
-        "ekatalog_total_produk_online" => $response[$a]["total_product"],
-        "ekatalog_status_paket_online" => $response[$a]["latest_status"]["status_paket"],
-        "ekatalog_tgl_buat_online" => $response[$a]["created_date"],
-        "ekatalog_tgl_ubah_online" => $response[$a]["modified_date"]
-      );
-      $id_pk_ekatalog = insertRow("mstr_ekatalog", $data);
+    $count = 0;
+    while (true) {
+      $count++;
+      $page = 20;
+      $url = "https://e-katalog.lkpp.go.id/purchasing/paket?keyword=&commodity=0&position=&negotiation=&year=2021&status=&sortby=desc&per_page=$page&offset=$count";
+      #$url = "http://localhost/mak/data/test_ekatalog.html";
 
-      $url = "https://e-katalog.lkpp.go.id/api/purchasing/api/v2/crud/paket/" . $response[$a]["id"];
       $curl = curl_init();
       curl_setopt_array($curl, array(
         CURLOPT_URL => $url,
@@ -174,52 +92,50 @@ class Sch_ekatalog extends CI_Controller
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "GET",
         CURLOPT_HTTPHEADER => array(
-          "Cookie: " . $this->login_cookies,
-          "Jwt-Token: " . $this->jwt,
+          "Cookie: " . $this->login_cookies
         )
       ));
-      $response2 = (curl_exec($curl)); #string
-      $response2 = json_decode($response2, true);
-      $kurs = $response2["data"]["paket"]["kurs"]["nama_kurs"];
-      $response2 = $response2["data"]["paket"]["products"];
-      $total = 0;
-      for ($b = 0; $b < count($response2); $b++) {
+      $response = (curl_exec($curl)); #string
+      $response = str_replace('<table aria-describedby="mydesc" class="table table-striped" id="tblListPaket">', '<table>', $response);
+      #echo $response;
+      $split = explode('<table>', $response);
+      $split = explode('</table>', $split[1])[0];
+      $split = explode('<tbody>', $split);
+      $split = explode('</tbody>', $split[1])[0];
 
-        $data = array(
-          "ekatalog_produk_nama_produk" => $response2[$b]["nama_produk"],
-          "ekatalog_produk_nama_produk_online" => $response2[$b]["nama_produk"],
-          "ekatalog_produk_kuantitas_online" => $response2[$b]["kuantitas"],
-          "ekatalog_produk_kuantitas" => $response2[$b]["kuantitas"],
-          "ekatalog_produk_mata_uang_online" => $kurs,
-          "ekatalog_produk_mata_uang" => $kurs,
-          "ekatalog_produk_harga_satuan_online" => $response2[$b]["harga_satuan"],
-          "ekatalog_produk_harga_satuan" => $response2[$b]["harga_satuan"],
-          "ekatalog_produk_perkiraan_ongkos_kirim_online" => $response2[$b]["harga_ongkir"],
-          "ekatalog_produk_perkiraan_ongkos_kirim" => $response2[$b]["harga_ongkir"],
-          "ekatalog_produk_total_harga_online" => $response2[$b]["harga_total"],
-          "ekatalog_produk_total_harga" => $response2[$b]["harga_total"],
-          "ekatalog_produk_catatan" => $response2[$b]["catatan"],
-          "ekatalog_produk_catatan_online" => $response2[$b]["catatan"],
-          "ekatalog_produk_status" => "aktif",
-          "ekatalog_produk_tgl_create" => $response2[$b]["created_date"],
-          "ekatalog_produk_id_create" => $response2[$b]["modified_date"],
-          "id_fk_ekatalog" => $id_pk_ekatalog,
+      $split = explode("<tr>", $split);
+      echo "Queried data: " . count($split) . " url: " . $url;
+      for ($a = 1; $a < count($split); $a++) {
+        $regex_link = "/(\/[a-z]+)+(\/[0-9]+)/";
+        preg_match($regex_link, $split[$a], $matches);
+        $link = $matches[0];
+
+        $regex_link_id = "/([0-9]){7}/"; #contoh id 3809900
+        preg_match($regex_link_id, $link, $matches);
+        $id = $matches[0];
+
+        $where = array(
+          "ekatalog_id" => $id,
+          "ekatalog_id_status_query" => 0
         );
-        insertRow("tbl_ekatalog_produk", $data);
-        $total += (int) $response2[$b]["konversi_harga_total"];
+        if (!isExistsInTable("temp_ekatalog_id", $where)) {
+          $data = array(
+            "ekatalog_id_link" => $link,
+            "ekatalog_id" => $id,
+            "ekatalog_id_status_query" => 0,
+          );
+          insertRow("temp_ekatalog_id", $data);
+        }
       }
-      echo $total."<br/><br/>";
       $data = array(
-        "ekatalog_total_harga" => $total
+        "log_auto_ekatalog" => "Mengambil daftar E-Katalog",
+        "log_auto_ekatalog_desc" => "Mengambil $page data terakhir dari e-katalog",
+        "log_auto_ekatalog_date" => date("Y-m-d H:i:s"),
       );
-      $where = array(
-        "id_pk_ekatalog" => $id_pk_ekatalog
-      );
-      updateRow("mstr_ekatalog", $data, $where);
-      
-      $sql = "
-      delete from tbl_ekatalog_produk where id_fk_ekatalog not in (select id_pk_ekatalog from mstr_ekatalog)";
-      executeQuery($sql);
+      insertRow("log_auto_ekatalog", $data);
+      if (count($split) < $page) {
+        break;
+      }
     }
   }
   public function get_ekatalog_detail()
@@ -345,8 +261,8 @@ class Sch_ekatalog extends CI_Controller
         $sql = "delete from mstr_ekatalog where ekatalog_id_paket = ? and ekatalog_tgl_update is null and ekatalog_id_create = 0";
         executeQuery($sql, $args); #delete sirup yang udah kedaftar di mstr sirup supaya prevent duplicate. However, setiap kali data masuk ke mstr_sirup, itu sudah pasti ada backupnya di archieve.
         $sql = "delete tbl_ekatalog_produk from tbl_ekatalog_produk inner join mstr_ekatalog on mstr_ekatalog.id_pk_ekatalog = tbl_ekatalog_produk.id_fk_ekatalog where ekatalog_id_paket = ?";
-        executeQuery($sql, $args);
-
+        executeQuery($sql,$args);
+        
         $matches[8] = $this->change_date($matches[8]);
         $matches[9] = $this->change_date($matches[9]);
         $total_harga = trim(str_replace("Rp ", "", str_replace(".", "", str_replace(",00", "", strip_tags($matches[12])))));
